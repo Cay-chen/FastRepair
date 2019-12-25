83918e55a48b6448d28db03fd9d0ee01434885a0
cay <cay@cays-MacBook-Air.local>
2019-12-25 16:56:20 +0800

    Initial commit

---
+++ app/src/main/java/com/fast/repair/biz/FastRepairApplication.java
@@ -0,0 +1,9 @@
+package com.fast.repair.biz;
+
+import android.app.Application;
+
+import com.scie.motse.base.container.BaseApplication;
+
+public class FastRepairApplication extends BaseApplication {
+
+}
---
+++ app/src/main/java/com/fast/repair/biz/login/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.fast.repair.biz.login;
+
+import com.fast.repair.base.container.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+}
